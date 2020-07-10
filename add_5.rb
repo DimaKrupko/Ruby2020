faff5bf265823aa4d9ab7d27aa35a6a736430646
DimaKrupko <67634675+DimaKrupko@users.noreply.github.com>
2020-07-11 01:14:47 +0300

    Add files via upload

---
+++ add_5.rb
@@ -0,0 +1,14 @@
+﻿    puts "Введіть три числа через пробіл"
+    chysla = gets.chomp
+    chysla = chysla.split (" ")
+    m=chysla.sort { |x,y| y<=>x }
+    for i in 0..2 do
+        k=m[i].to_i
+        if (k>10) || (k<1)
+            puts "Веддіть числа від 1 до 10"
+        end
+    end
+    a, b, c =m[0].to_i, m[1].to_i, m[2].to_i
+    max = (b+c)*a
+    puts max
+        
---
+++ add_6.rb
@@ -0,0 +1,14 @@
+﻿    puts "Введіть числа через пробіл"
+    s = gets.chomp
+    s = s.split (" ")
+    n = s.length
+    c = 0
+    for i in 0..n-1 do
+        e = s[i].to_i
+        if e == 0
+            c+=1
+        end
+    end 
+    v=n-c
+    puts v
+    
